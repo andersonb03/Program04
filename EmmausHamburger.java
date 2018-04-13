052d1c97770cd964c32a5b4563ff0e8fb2eb0688
Tim <fauxt@moravian.edu>
2018-04-13 15:06:44 -0400

    added emmaus sandwich work

---
+++ EmmausHamburger.java
@@ -0,0 +1,10 @@
+public class EmmausHamburger extends sandwich {
+    
+    public Bread createBread() {
+        return new Bun();
+    }
+    
+    public void makeSandwich(){
+	wrap = SandwichIngredientFactory.
+    }
+}
--- SchaperDeli.java
+++ SchaperDeli.java
@@ -5,6 +5,7 @@ public abstract class SchaperDeli {
     public Sandwich orderSandwich(String type) {
         Sandwich sandwich = createSandwich(type);
         System.out.println("--- Making a " + sandwich.getName() + " ---");
+	 sandwich.makeSandwich();
         return sandwich;
     }
 }
