8011fb7361e7bf7ceac038e31406eaac0d2110f9
Tim <fauxt@moravian.edu>
2018-04-20 13:09:15 -0400

    some cash money stuff

--- AllentownHamburger.java
+++ AllentownHamburger.java
@@ -10,7 +10,21 @@ public class AllentownHamburger extends Sandwich {
     }
 
     public double getCost() {
-        return 2.00;
+	 double totalCost = 2.0;
+        if (bread != null) {
+            totalCost += bread.getCost();
+        }
+        if (topping != null) {
+            for (int i = 0; i < topping.length; i++) {
+                totalCost += topping[i].getCost();
+            }
+        }
+        if (condiment != null) {
+            for (int i = 0; i < condiment.length; i++) {
+                totalCost += condiment[i].getCost();
+            }
+        }
+        return totalCost;
     }
     
     void makeSandwich() {
